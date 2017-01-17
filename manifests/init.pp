5414852768abb12cd65d16744be23d9936fdf0fa
root <root@ip-172-31-39-52.eu-west-1.compute.internal>
2017-01-17 08:25:31 -0500

    minor fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,12 +36,12 @@
 # Copyright 2017 Your name here, unless otherwise noted.
 #
 class apache (
-  $package_name = $::apache::params::package_name
+  $package_name = $::apache::params::package_name,
   $service_name = $::apache::params::service_name,
   $webpage_text = $::apache::params::webpage_text,
   ) inherits apache::params {
 
-  class { '::apapche::install': } ->
+  class { '::apache::install': } ->
   class { '::apache::config': } ~>
   class { '::apache::service': } ->
   Class['::apache']
